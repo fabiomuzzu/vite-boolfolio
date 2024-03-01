a8637c5c733de7f6abb8361d8104d79f274b8dc1
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-03-01 17:28:30 +0100

    [FIX] added scaffholding, completed milestone 3

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
-<script setup>
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+import {store} from './store.js';
+import axios from 'axios';
 
+export default {
+  data() {
+        return {
+            store,
+            projects: [],
+        }
+    },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
 </script>
 
 <template>
 
+<AppHeader/>
+<AppMain/>
+<AppFooter/>
+
 </template>
 
 <style scoped lang="scss">
-
+@use './style/generals.scss' as *;
+@use './style/partials/variables' as *;
 </style>
