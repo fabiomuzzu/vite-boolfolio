c1ff22855239e43265b82bbf7b43fb7e56f7152d
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-03-01 17:43:57 +0100

    added axios call to get api infos, completed milestone 4

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import {store} from './store.js';
 import axios from 'axios';
 
 export default {
+  name: 'Projects',
   data() {
         return {
             store,
@@ -16,7 +17,18 @@ export default {
     AppHeader,
     AppMain,
     AppFooter,
-  }
+  },
+  created(){
+    this.getProjects();
+  },
+  methods: {
+    getProjects(){
+      axios.get(this.store.apiUrl).then((response) =>{
+        this.projects = response.data
+        console.log(this.projects);
+      })
+    }
+  },
 }
 </script>
 
