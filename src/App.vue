c955eb59c3bba77a72efdb9d111807937fa90f4e
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-03-04 17:58:29 +0100

    completed base excercise

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
 import ProjectCard from './components/ProjectCard.vue';
 import AppFooter from './components/AppFooter.vue';
 import {store} from './store.js';
@@ -11,29 +10,15 @@ export default {
   data() {
         return {
             store,
-            projects: [],
         }
     },
 
   components: {
     AppHeader,
-    AppMain,
-    ProjectCard,
     AppFooter,
   },
   
-  created(){
-    this.getProjects();
-  },
-
-  methods: {
-    getProjects(){
-      axios.get(this.store.apiUrl).then((response) =>{
-        this.projects = response.data.results;
-        console.log(this.projects);
-      })
-    }
-  },
+  
 }
 </script>
 
@@ -41,11 +26,6 @@ export default {
 
 <AppHeader />
 <router-view></router-view>
-<div class="container">
-  <div class="row">
-    <ProjectCard v-for="(project, index) in projects" :key="index" :project="project" />
-  </div>
-</div>
 <AppFooter />
 
 </template>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,12 +1,44 @@
 <script>
+import ProjectCard from '../components/ProjectCard.vue';
+import { store } from '../store.js';
+import axios from 'axios';
+
 export default {
   name: "Projects",
+    data() {
+        return {
+            store,
+            projects: [],
+        }
+    },
+
+    components: {
+        ProjectCard,
+    },
+    
+    created(){
+        this.getProjects();
+    },
+
+    methods: {
+        getProjects(){
+        axios.get(this.store.apiUrl).then((response) =>{
+            this.projects = response.data.results;
+            console.log(this.projects);
+        })
+        }
+    },
+  
 };
 </script>
 
 <template lang="">
   <div> 
-      <h1 class="text-center">AAAAA</h1>
+    <div class="container">
+        <div class="row">
+            <ProjectCard v-for="(project, index) in projects" :key="index" :project="project" />
+        </div>
+    </div>  
   </div>
 </template>
 
